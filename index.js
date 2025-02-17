173b76ba2f0639cabc5eb592029ba15226311a1c
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2025-02-17 17:42:16 +0500

    done till var let and const

---
+++ index.js
@@ -0,0 +1,23 @@
+/*Alerts
+alerts are used as a pop up message on the website that gives a message to the user*/
+//Example
+//alert("welcome to javascript")
+
+// ****************const,let and var*****************
+/*const a=12;
+const b=12;
+const sum=a+b;
+alert("the sum of a and b is " + "" + sum)*?*/
+//Declaration can be done as follow
+// let age;
+// age=25;
+// console.log(age)
+// var name;
+// name="ali"
+// console.log(name)
+// let marks=70;
+// marks=marks+20;
+// console.log(marks)
+
+// *****************math Expressions in JS**********************
+
