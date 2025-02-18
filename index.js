9a5babd52ff96eb7c09426acf39cafd3cf02c6bf
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2025-02-18 15:29:22 +0500

    operators done with strings

--- index.js
+++ index.js
@@ -20,4 +20,24 @@ alert("the sum of a and b is " + "" + sum)*?*/
 // console.log(marks)
 
 // *****************math Expressions in JS**********************
+// const a= 5;
+// const b=6;
+// const sum=a+b;
+// console.log(sum)
+
+// Now if we add a sumber and a string.
+// const a=2;
+// const b= "34";
+// const sum= a+b;
+// console.log(sum)
+
+// similarly, if we do subtraction, multiplication and division of a num and string it will be accepted as nums.
+// const a = 34;
+// const b = "456";
+// const sub = b - a;
+// const mul = a*b;
+// const div = b/a;
+// console.log(sub)
+// console.log(mul)
+// console.log(div)
 
