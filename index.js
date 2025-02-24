9df7f81db89df51ef79b9347f218faf7a10e7140
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2025-02-24 16:05:45 +0500

    done with ch#7 mathematical expressions eleminating ambiguity

--- index.js
+++ index.js
@@ -218,3 +218,4 @@ alert("the sum of a and b is " + "" + sum)*?*/
 // Done with ch#4 quiz (var names legal and illegal)
 // Done with ch#5 quiz (mathematical expressions)
 // Done with ch#6 quiz (unfamiliar operators)
+// Done with ch#7 quiz (math expressions elimination ambiguity)
