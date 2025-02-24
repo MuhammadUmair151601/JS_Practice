762fdc13a47d657c5303fa0b4d4cc3adcdd40cab
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2025-02-24 15:32:11 +0500

    done with ch#5 quiz mathematical expressions

--- index.js
+++ index.js
@@ -216,4 +216,4 @@ alert("the sum of a and b is " + "" + sum)*?*/
 // Done with ch#2 quiz (strings)
 // Done with ch#3 quiz (variable of numbers)
 // Done with ch#4 quiz (var names legal and illegal)
-// 
+// Done with ch#5 quiz (mathematical expressions)
